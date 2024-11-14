02da6df0a133dbe11b1b6ce73dd6f8b4f8630fa5
Willpatbarr <145700970+Willpatbarr@users.noreply.github.com>
2024-11-14 13:20:05 -0700

    Initial Commit

---
+++ MarketBase/MarketBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketBaseApp.swift
+//  MarketBase
+//
+//  Created by William Barr on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MarketBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarketBaseTests/MarketBaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarketBaseTests.swift
+//  MarketBaseTests
+//
+//  Created by William Barr on 11/14/24.
+//
+
+import Testing
+@testable import MarketBase
+
+struct MarketBaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
